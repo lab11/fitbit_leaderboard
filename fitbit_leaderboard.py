a4b28b593e15226569c3c9b1958450deb4a2c774
William Huang <wwhuang@wwhuang-OptiPlex-980.(none)>
2013-02-14 04:54:33 -0500

    d3 getting data from fitbit. Need to add everyone

--- fitbit_leaderboard.py
+++ fitbit_leaderboard.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 import oauth2 as oauth
 import sqlite3
 
-from flask import Flask, render_template, request, g, make_response
+from flask import Flask, render_template, request, g, make_response, jsonify
 import fitbit
 
 #config 
