ed378069fede70f3cc93a63e306c5ef73897e53c
Brad Campbell <bradjc@umich.edu>
2013-02-15 18:07:00 -0500

    adding in db and fitbit manager

--- fitbit_leaderboard.py
+++ fitbit_leaderboard.py
@@ -2,89 +2,47 @@ from __future__ import with_statement
 from contextlib import closing
 from datetime import datetime, date
 import oauth2 as oauth
-import sqlite3
-import json 
+import json
 
 from flask import Flask, render_template, request, g, make_response, jsonify
-import fitbit
 
-#config 
+from db import fitbit_db
+import fitbit_manager
+
+#config
 DATABASE = 'fitbit.db'
 DEBUG = True
 SECRET_KEY = 'dev key'
 USERNAME = 'admin'
 PASSWORD = 'sharedspace'
-consumer_key = '4f0defd304af44e9a6790b0087070313'
-consumer_secret = '737d767bf5024fda928ea039d77e1098'
-
-day_converter = {
-	0: 'm', 
-	1: 't', 
-	2: 'w',
-	3: 'th',
-	4: 'f',
-	5: 's', 
-	6: 'sun'
-}
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-uauth_client = fitbit.Fitbit('4f0defd304af44e9a6790b0087070313', ' 737d767bf5024fda928ea039d77e1098 ')
+db = fitbit_db.fitbit_db('fitbit.db')
+fm = fitbit_manager.fitbit_manager(db)
 
-def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
-
-def query_db(query, args=(), one=False):
-	cur = g.db.execute(query, args)
-	rv = [dict((cur.description[idx][0], value)
-			for idx, value in enumerate(row)) for row in cur.fetchall()]
-	return (rv[0] if rv else None) if one else rv
-
-# Right now just returns all user step info for the last week. Needs to filter by group (4908, cse, etc)
-def get_group_info(): 
-	user_cur = g.db.execute('select username, fitbit_user_key, fitbit_user_secret from user order by user_id desc')
-	users = [dict(username=row[0], fitbit_user_key=row[1], fitbit_user_secret=row[2]) for row in user_cur.fetchall()]
-	data = []
-	for user in users: 
-		oauth_fitbit = fitbit.Fitbit(consumer_key, consumer_secret, user_key=user['fitbit_user_key'], user_secret=user['fitbit_user_secret'])
-		step_response = oauth_fitbit.time_series('activities/steps', period='1w')
-		user_info = {}
-		user_info['username'] = user['username'].encode('ascii')
-		user_info['total_steps'] = 0
-		user_info['step_counts'] = []
-		for day in step_response['activities-steps']:
-			mdate = datetime.strptime(day['dateTime'], "%Y-%m-%d")
-			mweekday = day_converter[mdate.weekday()]
-			user_info['step_counts'].append( { 'day':mweekday, 'steps':int(day['value']) } )
-			user_info['total_steps'] += int(day['value'])
-		data.append(user_info)
-
-	data = sorted(data, key=lambda user_info: user_info['total_steps'], reverse=True)
-	return data
+# Do an initial update to get a weeks worth of data at the least
+fm.update(7)
 
 
 @app.before_request
 def before_request():
-	g.db = connect_db()
+#	g.db = connect_db()
+	pass
 
 @app.teardown_request
 def teardown_request(exception):
-	if hasattr(g, 'db'):
-		g.db.close()
+#	if hasattr(g, 'db'):
+#		g.db.close()
+	pass
 
 @app.route('/')
-def home(): 
+def home():
 	return render_template('home.html')
 
 @app.route('/register')
-def register(): 
+def register():
 	request_token = uauth_client.client.fetch_request_token()
 	fitbit_auth_url = uauth_client.client.authorize_token_url(request_token)
 	response = make_response( render_template('register.html', fitbit_auth_url=fitbit_auth_url) )
@@ -109,23 +67,25 @@ def registered():
 			g.db.execute('insert into user (username, fitbit_verifier, fitbit_user_key, fitbit_user_secret) values (?, ?, ?, ?)', [request.form['username'], request.form['fitbit_verifier'], user_token.key, user_token.secret])
 			g.db.commit()
 		return render_template('registered.html')
-	else: 
-		return "Method error" 
+	else:
+		return "Method error"
 
-@app.route("/show_users") 
+@app.route("/show_users")
 def show_users():
-	cur = g.db.execute('select username, fitbit_verifier, fitbit_user_key, fitbit_user_secret from user order by user_id desc')
-	users = [dict(username=row[0], fitbit_verifier=row[1], fitbit_user_key=row[2], fitbit_user_secret=row[3]) for row in cur.fetchall()]
+	db = fitbit_db.fitbit_db('fitbit.db')
+	users = db.get_users()
 	return render_template('show_users.html', users=users)
 
 @app.route("/group_info")
-def group_info(): 
-	data = get_group_info()
+def group_info():
+	fm.set_db(fitbit_db.fitbit_db('fitbit.db'))
+	data = fm.retrieve()
 	return json.dumps(data)
 
 @app.route("/leaderboard")
 def leaderboard():
-	data = get_group_info()
+	fm.set_db(fitbit_db.fitbit_db('fitbit.db'))
+	data = fm.retrieve()
 	return render_template('leaderboard.html', data=data)
 
 if __name__ == '__main__':
